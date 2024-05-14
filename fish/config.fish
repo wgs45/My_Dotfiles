f7654ce7925e22aa19ca852f3f9cda92135c8877
W45 <73011833+wgs45@users.noreply.github.com>
2024-05-14 11:44:48 +0800

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 function fish_greeting
-neofetch
+    neofetch
 end
 funcsave fish_greeting
 
@@ -17,11 +17,22 @@ alias ls "ls -p -G"
 alias la "ls -A"
 alias ll "eza --long --icons --group --git --header --created --modified --no-user"
 alias lt "eza --tree --icons --group --git --long --level=2 --header --created --modified --no-user"
-alias bat "bat"
-alias rp "rip"
+alias bat bat
+alias rp rip
 alias rpu "rip -u"
+alias fix fuck
+
+# aliases for tmux
+alias txn "tmux neww"
+alias txk "tmux kill-session"
+alias txd "tmux detach"
+alias txl "tmux list-sessions"
+
+# Others
 # alias scc "scc"
 # alias http "http"
 # alias z "z"
 # alias rg "rg" ripgrep
 # fuzzy finder (fzf) installed using fish
+
+thefuck --alias | source
